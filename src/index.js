14fcc11ca0f42ad8f82725f0be7b810df0867f32
Flame <5017_30@mail.ru>
2023-09-18 19:39:55 +0300

    add: create, getAll, getOne, remove, update posts

--- src/index.js
+++ src/index.js
@@ -1,31 +1,46 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import { loginValidation, registerValidation } from './validation/auth.js'
-import checkAuth from './utils/checkAuth.js'
-import { getMe, login, register } from './controlers/UserController.js'
+import express from "express";
+import mongoose from "mongoose";
+import {
+  loginValidation,
+  postCreateValidation,
+  registerValidation,
+} from "./validation/validation.js";
+import checkAuth from "./utils/checkAuth.js";
+import { getMe, login, register } from "./controlers/UserController.js";
+import {
+  create,
+  getAll,
+  getOne,
+  remove,
+  update,
+} from "./controlers/PostController.js";
 
 mongoose
-  .connect('mongodb://localhost/blog')
+  .connect("mongodb://localhost/blog")
   .then(() => {
-    console.log('DB Ok')
+    console.log("DB Ok");
   })
   .catch((err) => {
-    console.error('DB ERROR', err)
-  })
+    console.error("DB ERROR", err);
+  });
 
-const app = express()
+const app = express();
 
-app.use(express.json()) // чтобы читать json
+app.use(express.json()); // чтобы читать json
 
-app.post('/auth/register', registerValidation, register)
+app.post("/auth/register", registerValidation, register);
+app.post("/auth/login", loginValidation, login);
+app.get("/auth/me", checkAuth, getMe);
 
-app.post('/auth/login', login)
-
-app.get('/auth/me', checkAuth, getMe)
+app.get("/posts", getAll);
+app.get("/posts/:id", getOne);
+app.post("/posts", checkAuth, postCreateValidation, create);
+app.delete("/posts/:id", checkAuth, remove);
+app.patch("/posts/:id", checkAuth, update);
 
 app.listen(4444, (err) => {
   if (err) {
-    return console.error(err)
+    return console.error(err);
   }
-  console.log('Server OK. Port: http://localhost:4444')
-})
+  console.log("Server OK. Port: http://localhost:4444");
+});
